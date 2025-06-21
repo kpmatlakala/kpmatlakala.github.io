66949089e10969a976bac1564ee50d7d8c596107
Kabelo Matlakala <matlakalakabelo1@gmail.com>
2025-06-21 13:00:15 +0200

    update portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -8,41 +8,9 @@ import ChatModal from "./components/Chat/ChatModal";
 import FloatingGameButton from "./components/Buttons/FloatingGameButton/FloatingGameButton";
 
 function App() 
-{
-  const [showSplash, setShowSplash] = useState(false);
+{  
   const [chatMinimized, setChatMinimized] = useState(true);
   const [chatMaximized, setChatMaximized] = useState(false);
-
-  if(localStorage.getItem("firstMessageShown") && !localStorage.getItem("userName"))
-  {
-      localStorage.clear();
-      window.location.reload();
-  }
-
-  // useEffect(() => {
-  //   const userName = localStorage.getItem("userName");
-  //   const sess_complete = localStorage.getItem("session_complete");
-
-  //     if(userName)
-  //     {
-  //       if(sess_complete)
-  //       {
-  //         setShowSplash(false);
-  //         setChatMinimized(true);
-  //       }        
-  //     }
-  //     else
-  //     {
-  //         setShowSplash(true);
-  //         setChatMinimized(false);
-  //     }
-    
-  // }, [])
-  
-  const handleSplashFinish = () => {
-    setShowSplash(false);
-    setChatMinimized(true);  // Minimize after splash screen
-  };
 
   const handleChatToggle = () => {
     setChatMinimized(!chatMinimized);
